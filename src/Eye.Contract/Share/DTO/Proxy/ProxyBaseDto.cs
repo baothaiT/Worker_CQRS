c8ff6a5185b68065c8e901148e7861f61d8d099d
DESKTOP-D1B32EL\bao <thaibao0225@gmail.com>
2024-10-01 01:51:10 +0700

    Update Dto

--- src/Eye.Contract/Share/DTO/Proxy/ProxyBaseDto.cs
+++ src/Eye.Contract/Share/DTO/Proxy/ProxyBaseDto.cs
@@ -14,4 +14,8 @@ public class ProxyBaseDto
     public string User { get; set; }
     public string Password { get; set; }
     public ProxyStatusEnum? IsStatus { get; set; }
+    public bool? IsMigration { get; set; }
+    public DateTime? CreateDate { get; set; }
+    public DateTime? UpdateDate { get; set; }
+    public DateTime? CheckLiveDate { get; set; }
 }
